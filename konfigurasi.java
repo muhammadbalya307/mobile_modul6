76cb4a3cc095406cf4b01802cbe14c2d48343af7
muhammad balya <50208607+muhammadbalya307@users.noreply.github.com>
2020-05-15 07:12:34 +0700

    Add files via upload

---
+++ konfigurasi.java
@@ -0,0 +1,27 @@
+package com.example.mymodul6;
+
+public class konfigurasi {
+
+    public static final String URL_ADD = "http://192.168.43.182/android/insert.php";
+    public static final String URL_GET_ALL = "http://192.168.43.182/android/read.php";
+    public static final String URL_GET_MHS = "http://192.168.43.182/android/select.php?id=";
+    public static final String URL_UPDATE_MHS = "http://192.168.43.182/android/update.php";
+    public static final String URL_DELETE_MHS = "http://192.168.43.182/android/delete.php?id=";
+
+
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+
+    //ID
+    public static final String MHS_ID = "mhs_id";
+}
+
